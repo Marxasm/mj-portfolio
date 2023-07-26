f399e50790223c135931cd85526bbd7bf850d0c6
Marxasm <marcjunellezamora@gmail.com>
2023-07-26 17:12:35 +0800

    new project added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,25 +1,28 @@
- 
-import RestCountries from "./../assets/portfolioImages/restcountries.jpg";
-import SpaceTourism from "./../assets/portfolioImages/spacetourism.jpg";
-import Advice from "./../assets/portfolioImages/advice.jpg";
-import Page from "./../assets/portfolioImages/intropage.jpg";
-// import AWS from "./../assets/portfolioImages/s3.PNG";
-import Todo from "./../assets/portfolioImages/todo.jpg"
-
-// in progress projects
-
-import sb from './../assets/portfolioImages/sb.png'
+ import sb from './../assets/portfolioImages/sb.png'
+import pdrrmo from './../assets/portfolioImages/pdrrmo.png'
  
 const ProjectsData = [
+
   {
-    id: "fyp",
+    id: "invty",
+    img: pdrrmo,
+    name: "PDRRMO Warehouse Management System",
+    stack: ["< CodeIgniter 3/>", "< Bootstrap />", "< MySQL />", "< PHP />", "< AdminLTE />"],
+    live: "https://inventorypdrrmo.000webhostapp.com/",
+    source: "https://github.com/Marxasm/inventory",
+    description:  
+        "A Provincial Disaster Risk Reduction and Management Office warehouse management system that handles the items and tools information and monitors the quantity per item. It also has a log that can record the borrowers' actions, complete register and login system, admin restrictions and control, CRUD functions, export reports, and view history. (Demo account: user: demosample pass: Sample112233)",
+  }, 
+
+  {
+    id: "sb",
     img: sb,
-    name: "Sangguniang Bayan Document Management System",
+    name: "Sangguniang Bayan Information Management System",
     stack: ["< CodeIgniter 3/>", "< Bootstrap />", "< MySQL />", "< PHP />", "< AdminLTE />"],
     live: "https://sangguniangbayan.000webhostapp.com/",
-    source: "https://github.com/Marxasm/SB/tree/master",
+    source: "https://github.com/Marxasm/SB",
     description:  
-        "A management system that inputs important documents, and act as a log to monitor ingoing and outgoing documents given to employees.",
+        "A management system that inputs important documents, and act as a log to monitor ingoing and outgoing documents given to employees. It has simple login and register system, email system, CRUD functions, data tables, and export reports. (Demo account: user: demosample pass: Sample332211)",
   }, 
   
    
