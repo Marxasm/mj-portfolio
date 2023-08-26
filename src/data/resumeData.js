ea2ca64efd74c8b9531d6bd10b1fc1ed75d62b81
Marxasm <marcjunellezamora@gmail.com>
2023-08-26 15:26:52 +0800

    resume commit

--- src/data/resumeData.js
+++ src/data/resumeData.js
@@ -2,7 +2,7 @@ const resumeData = [
    
   {
     id: 1,
-    source: 'https://drive.google.com/file/d/1q-nBcPiPszPxRwOYKxJtRtFUlOuIyfPB/view?usp=drive_link',
+    source: 'https://drive.google.com/file/d/1L_ekWjq85hT8vSUvbwThl7THuU9seUNG/view?usp=sharing',
   },  
 
 
