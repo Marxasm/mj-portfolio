6f4909b5cea0afa1c9df20131cf6688e6d09caa7
Marxasm <marcjunellezamora@gmail.com>
2024-05-04 20:59:56 +0800

    May 2024 test

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,11 +1,29 @@
 const expData = [
+
+  {
+    id: "1",
+    org: "OnlineJobs",
+    title: "Freelance Virtual Assistant",
+    date: "December 2023 – Present",
+    description: <p>Developed a JavaScript-based free tool calculator for gaming enthusiasts, creating complex calculations.</p>,
+  },  
    
     {
-      id: "1",
-      org: "MABROX OUTSOURCING SERVICES",
+      id: "2",
+      org: "Mabrox Outsourcing Services",
       title: "Customer Service Associate",
       date: "Oct 2022 - June 2023",
       description: <p>Worked in cold calls, virtual assistance, digital marketing, SEO practices, and appointment setting.</p>,
+    },  
+
+    {
+      id: "3",
+      org: "Provincial Disaster Risk Reduction and Management Office",
+      title: "IT Intern",
+      date: "March – May 2022",
+      description: <p>Implemented web development by creating and presenting an offline Warehouse Management System website
+      using the CodeIgniter PHP Framework.
+      </p>,
     },  
 
     
